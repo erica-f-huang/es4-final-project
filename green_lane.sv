db1e5b7e51e6b0d91b944fec81a63667e2913f9c
Erica Huang <Erica.Huang@tufts.edu>
2025-12-05 15:46:34 -0500

    READS IN NOTES YAY. needs to be high for 7 cycle to drop a full block bc it reads it every cycle and the block is 7 registers long

--- green_lane.sv
+++ green_lane.sv
@@ -20,44 +20,44 @@ always_comb begin
                         lane_rgb = 6'b001100;
                 end
         end
-        // for (int i = 0; i < 96; i++) begin
-        //         if (shift_reg[i] == 1) begin     // or != '0 if multi-bit
-        //                 if(((i*5) <= row) & row < (i*5 + 5)) begin
-        //                         lane_rgb = 6'b001100;
-        //                 end else begin
-        //                         lane_rgb = 6'd0;
-        //                 end
-        //         end else begin
-        //                 lane_rgb = 6'd0;
-        //         end
-        // end
 end
-
-
 
 logic shift_reg [96]; //96 shift regs
 logic [3:0] pulse_counter = 0;
+logic [20:0] note = 21'b111111100111111100000;
+logic [5:0] which_note = 0;
+
+/* NOTE
+instead of having a pulse counter just hardcode the 5bit signals to be hig for 7 cycles and to have at leats 1 cycle in between highs
+*/
 
 always_ff @(posedge clk) begin
-        //click button and it stays high for 7 cycles
-        if ((button == 1'b0) || ((pulse_counter % 8) != 0)) begin   
-                // set 1'b1 for a single-pulse block
+        if(note[which_note%21] == 1) begin
                 shift_reg[0] <= 1;
-                pulse_counter <= pulse_counter + 1;
         end else begin
-                pulse_counter <= 0;
                 shift_reg[0] <= 0;
         end
 
-        /*
-        - CHECK THIS EVERY 7 CLOCK CYCLES SO IT PUTS 7 5PIXEL SEGMENTS AT ONCE TO CREATE A BLOCK
-        - but then it will also be low for 7 cycles
-        - nice to haveitis
-        */
+        if(which_note == 5'd21) begin
+                which_note = 0;
+        end else begin
+                which_note <= which_note + 1;
+        end
+
+        
+        // if((pulse_counter % 8) != 0) begin
+        //         if (button == 1'b0) begin   // set 1'b1 for a single-pulse block
+        //                 shift_reg[0] <= 1;
+        //         end else begin
+        //                 shift_reg[0] <= 0;
+        //         end
+        // end else begin
+        //         shift_reg[0] <= 0;
+        // end
         
 end
 
-// Generate the remaining frames
+// Generate the shift registers for the frames
 genvar i;
 generate
 for (i = 1; i < 96; i++) begin : gen_shift
